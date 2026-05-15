9cf7fccdac60c1c63a04659fc841d46a04101a15
prishsha <prishavadhavkar@gmail.com>
2026-05-15 19:21:53 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import Game from "./components/Game";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Game />
+    </div>
+  );
+}
+
+export default App;
